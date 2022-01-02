bb96031fe9e4fbc95d265df48656319247088f86
ErnestoVera <veraem.sw@gmail.com>
2022-01-02 02:26:37 -0800

    corregido detalle con la formula con la que se calculaban los senos,faltaba el factor 1/N en todas las expresiones del seno

--- sampling.py
+++ sampling.py
@@ -6,17 +6,17 @@ total_muestras = 32
 # Primera grafica, se conserva la frecuencia con sampling rate multiplo entero del periodo. 
 
 fig, (ax1, ax2) = plt.subplots(2, 1, constrained_layout = True)
-N = 1 # periodo
+N = 5 # periodo
 Ts = N/(total_muestras-1) # Sampling rate
 v = Ts/N # frecuencia normalizada.
 k = N*v
 ciclos = v*total_muestras # Total de ciclos.
 print(ciclos)
 n = np.arange(0, total_muestras, 1) # tiempo discreto 
-y = np.sin(2*np.pi*k*n) # funcion de tiempo discreto.
+y = np.sin(2*np.pi*k*n/N) # funcion de tiempo discreto.
 ax1.stem(Ts*n, y)
 t = np.arange(0, N, N/500)
-ax1.plot(t, np.sin(2*np.pi*t))
+ax1.plot(t, np.sin(2*np.pi*t/N))
 ax1.set_title("periodo/sampling rate entero.")
 ax1.set_xlabel("Ts*n")
 ax1.set_ylabel("y[n]")
@@ -27,10 +27,10 @@ k2 = N*v2
 ciclos2 = v2*total_muestras # Total de ciclos.
 print(ciclos2)
 n2 = np.arange(0, total_muestras, 1) # tiempo discreto 
-y2 = np.sin(2*np.pi*k2*n2) # funcion de tiempo discreto.
+y2 = np.sin(2*np.pi*k2*n2/N) # funcion de tiempo discreto.
 ax2.stem(Ts2*n, y2)
 t2 = np.arange(0, ciclos2*N, ciclos2*N/500)
-ax2.plot(t2, np.sin(2*np.pi*t2))
+ax2.plot(t2, np.sin(2*np.pi*t2/N))
 ax2.set_title("periodo/sampling rate racional.")
 ax2.set_xlabel("Ts*n")
 ax2.set_ylabel("y[n]")
