4aca5a593029b40b5dd85c922766c2fbaf5acca6
ErnestoVera <veraem.sw@gmail.com>
2022-01-02 02:11:42 -0800

    Agregadas en subplots, y con sus respectivos titulos y labels.

--- sampling.py
+++ sampling.py
@@ -1,11 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-total_muestras = 14
+total_muestras = 32
 
 # Primera grafica, se conserva la frecuencia con sampling rate multiplo entero del periodo. 
 
-plt.figure(1)
+fig, (ax1, ax2) = plt.subplots(2, 1, constrained_layout = True)
 N = 1 # periodo
 Ts = N/(total_muestras-1) # Sampling rate
 v = Ts/N # frecuencia normalizada.
@@ -14,20 +14,24 @@ ciclos = v*total_muestras # Total de ciclos.
 print(ciclos)
 n = np.arange(0, total_muestras, 1) # tiempo discreto 
 y = np.sin(2*np.pi*k*n) # funcion de tiempo discreto.
-plt.stem(Ts*n, y)
+ax1.stem(Ts*n, y)
 t = np.arange(0, N, N/500)
-plt.plot(t, np.sin(2*np.pi*t))
-
+ax1.plot(t, np.sin(2*np.pi*t))
+ax1.set_title("periodo/sampling rate entero.")
+ax1.set_xlabel("Ts*n")
+ax1.set_ylabel("y[n]")
 # Segunda grafica con sampling rate con numero racional.
-plt.figure(2)
-Ts2 = N/8.5 # Sampling rate
+Ts2 = N/4.5 # Sampling rate
 v2 = Ts2/N # frecuencia normalizada.
 k2 = N*v2
 ciclos2 = v2*total_muestras # Total de ciclos.
 print(ciclos2)
 n2 = np.arange(0, total_muestras, 1) # tiempo discreto 
 y2 = np.sin(2*np.pi*k2*n2) # funcion de tiempo discreto.
-plt.stem(Ts2*n, y2)
+ax2.stem(Ts2*n, y2)
 t2 = np.arange(0, ciclos2*N, ciclos2*N/500)
-plt.plot(t2, np.sin(2*np.pi*t2))
+ax2.plot(t2, np.sin(2*np.pi*t2))
+ax2.set_title("periodo/sampling rate racional.")
+ax2.set_xlabel("Ts*n")
+ax2.set_ylabel("y[n]")
 plt.show()
